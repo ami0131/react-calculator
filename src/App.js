a3b0786c794012eb92a51e50311205be39ac8d81
ami0131 <amirlan0131@gmail.com>
2024-01-09 18:49:11 +0800

    mai

---
+++ src/App.js
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <p>Hello ami</p>
+      <img src="https://www.google.com/url?sa=i&url=https%3A%2F%2Fwww.smithsonianmag.com%2Fsmart-news%2Fcats-react-to-baby-talk-from-their-owners-but-not-strangers-180981007%2F&psig=AOvVaw38R4TzdUn5pwuePsoFEQEc&ust=1704882508851000&source=images&cd=vfe&opi=89978449&ved=0CBEQjRxqFwoTCMjIrb2M0IMDFQAAAAAdAAAAABAD" />
+    </div>
+  );
+}
